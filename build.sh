aa3babcfed411df52fe83118e6875db1916b0ded
XGFan <XGFan@users.noreply.github.com>
2020-07-28 04:19:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ env GOOS=darwin GOARCH=amd64 go build -o ./build/stClient-mac ./client
 env GOOS=windows GOARCH=amd64 go build -o ./build/stServer-win.exe ./server
 env GOOS=linux GOARCH=amd64 go build -o ./build/stServer-linux ./server
 env GOOS=darwin GOARCH=amd64 go build -o ./build/stServer-mac ./server
-ls -alh ./build/
+ls -alh ./build
 mkdir -p output
 cp ./cfip.txt ./output/cfip.txt
 zip -q output/stServer-win.zip ./build/stServer-win.exe -j
@@ -14,4 +14,4 @@ zip -q output/stServer-linux.zip ./build/stServer-linux -j
 zip -q output/stClient-win.zip ./build/stClient-win.exe -j
 zip -q output/stClient-mac.zip ./build/stClient-mac -j
 zip -q output/stClient-linux.zip ./build/stClient-linux -j
-ls -alh ./output/
+ls -alh ./output
